18425b329972f82fbf17e523e73e5f7c00814ab5
half-but <qudcks9191@naver.com>
2017-10-22 23:52:56 +0900

    add content layout

--- app/src/main/java/root/onesms/Activity/SettingActivity.kt
+++ app/src/main/java/root/onesms/Activity/SettingActivity.kt
@@ -2,6 +2,14 @@ package root.onesms.Activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_setting.*
 import root.onesms.R
 
 class SettingActivity : AppCompatActivity() {
@@ -10,7 +18,56 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter =
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.menu_preview -> {
+                Log.d("hello world", "nice")
+                return true
+            }
+            R.id.menu_guide -> {
+                Log.d("hello world", "nice")
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    class SettingAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int) : RecyclerView.ViewHolder{
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return (4 + 2 + 2)
+        }
+
+        class ContentViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+        }
+
+        class HeaderViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+        }
+
+        class SwitchViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+        }
     }
 
 }
